18b7b8b99d2f4ea26330454fe7257efadf481b0b
Steve Baron <ndabasteve1@gmail.com>
2023-07-05 20:43:15 +0300

    Added styling to index.html

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ function rankData(data) {
     let result = data.sort((a, b) => b.votes - a.votes)
     result.forEach(element => {
         let locationDiv = document.createElement('div')
+        locationDiv.classList.add('locationDiv')
         document.querySelector('.Locations').appendChild(locationDiv)
 
         let locationName = document.createElement('h1')
@@ -14,6 +15,7 @@ function rankData(data) {
 
         let locationImage = document.createElement('img')
         locationImage.src = element.image
+        locationImage.classList.add('locationImage')
 
         let locationVotes = document.createElement('h2')
         locationVotes.votes = element.votes
@@ -21,10 +23,13 @@ function rankData(data) {
 
         let voteBtn = document.createElement('button')
         voteBtn.innerText = 'Vote'
+        voteBtn.classList.add('voteBtn')
+
 
         locationDiv.appendChild(locationName)
         locationDiv.appendChild(locationImage)
         locationDiv.appendChild(locationVotes)
+        locationDiv.appendChild(voteBtn)
 
 
 
